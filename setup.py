b54564e60a94545e3a845446c5c2440b7f5ce216
Jialin Cui <jialin@system76-pc.localdomain>
2020-08-08 14:05:58 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='SE',
+      version='1.0',
+      description='HW1',
+      author='Jialin Cui',
+      author_email='jcui9@ncsu.edu',
+      packages=['code'],
+     )
